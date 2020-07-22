c6374c9bfba814d2abe23f8be389e8292daabfa2
Azrilardian <irfan05ariell@gmail.com>
2020-07-22 09:18:00 +0800

    Update : Membuat fitur fillter, mengubah warna background

--- js/script.js
+++ js/script.js
@@ -1,88 +1,78 @@
 function list() {
     const ListValue = document.querySelector('.add input');
     const btnAdd = document.querySelector('.add button');
-    const listContainer = document.querySelector('.list-container');
+    const select = document.querySelector('select');
 
     btnAdd.addEventListener('click', add);
-    ListValue.addEventListener('keyup', e => {
-        if (e.keyCode == 13) add();
-    });
+    ListValue.addEventListener('keyup', e => e.keyCode == 13 ? add() : false);
+    document.addEventListener('click', e => removeAndCompleted(e));
+    select.addEventListener('click', e => fillter(e));
+
+    const removeAndCompleted = e => {
+        let target = e.target;
+        if (target.classList.contains('remove')) {
+            clickEffect(target);
+            removeList(target);
+            ListValue.focus();
+        } else if (target.classList.contains('done')) {
+            clickEffect(target);
+            doneList(target);
+            ListValue.focus();
+        };
+    };
+
+    const fillter = e => {
+        const todos = document.querySelectorAll('.list');
+        todos.forEach(todo => {
+            switch (e.target.value) {
+                case 'all':
+                    todo.style.display = 'flex';
+                    break;
+                case 'completed':
+                    todo.classList.contains('completed') ? todo.style.display = 'flex' : todo.style.display = 'none';
+                    break;
+                case 'uncompleted':
+                    !todo.classList.contains('completed') ? todo.style.display = 'flex' : todo.style.display = 'none';
+                    break;
+            };
+        });
+    };
 
     function add() {
         if (ListValue.value == "") return;
         else {
             createList();
-            changePlaceholderText(ListValue);
+            ListValue.setAttribute('placeholder', 'What next ?');
             ListValue.value = '';
+            ListValue.focus();
         }
     }
-
     const createList = () => {
+        const listContainer = document.querySelector('.list-container');
         const div = document.createElement('div');
         div.classList.add('list', 'loading');
         div.innerHTML = listElement();
         listContainer.appendChild(div);
-        setTimeout(() => {
-            div.classList.remove('loading');
-        }, 600)
+        setTimeout(() => { div.classList.remove('loading') }, 600);
     }
-
-    const changePlaceholderText = input => input.setAttribute('placeholder', 'Apa kegiatan selanjutnya ?');
-
-    function documentListener() {
-        const removeAlert = document.querySelector('.remove-list-alert');
-        const editAlert = document.querySelector('.edit-list-alert');
-        const textArea = document.querySelector('.edit-list-alert textarea');
-        const deleteBtn = document.querySelector('button#delete');
-        const cancelDeleteBtn = document.querySelector('button#no');
-        document.addEventListener('click', e => {
-            if (e.target.classList.contains('remove')) {
-                clickEffect(e.target);
-                showRemoveAlert(removeAlert);
-                cekOption(e.target, removeAlert, deleteBtn, cancelDeleteBtn, removeList);
-            } else if (e.target.classList.contains('edit')) {
-                clickEffect(e.target);
-                showEditAlert(e.target, editAlert, textArea);
-                textArea.focus();
-            }
-        });
-    };
-    documentListener();
 
     const removeList = target => {
         const list = target.parentElement.parentElement;
-        list.remove();
-    };
-    const showRemoveAlert = el => el.classList.add('active');
-    const hideRemoveAlert = el => el.classList.remove('active');
-
-    const showEditAlert = (target, el, textArea) => {
-        let targetValue = target.parentElement.previousElementSibling;
-        const saveEditBtn = document.querySelector('button#save');
-        const cancelEditBtn = document.querySelector('button#cancel');
-        const getInput = e => e.textContent = textArea.value;
-        textArea.value = targetValue.textContent;
-        el.classList.add('active');
-        cekOption(target, el, saveEditBtn, cancelEditBtn, getInput, targetValue);
+        list.classList.add('removed');
+        list.classList.remove('completed');
+        setTimeout(() => { list.remove() }, 600);
     };
 
-    const cekOption = (target, el, trueBtn, falseBtn, callback, targetValue) => {
-        trueBtn.addEventListener('click', () => {
-            clickEffect(target);
-            trueBtn.id == 'delete' ? callback(target) : callback(targetValue);
-            hideRemoveAlert(el);
-        });
-        falseBtn.addEventListener('click', () => {
-            clickEffect(target);
-            hideRemoveAlert(el)
-        });
-    };
+    const doneList = target => {
+        const list = target.parentElement.parentElement;
+        list.classList.toggle('completed');
+    }
 
     const listElement = () => {
         return `<p>${ListValue.value}</p>
         <div class="button">
-        <button class="edit"><i class="fas fa-pen"></i></button>
-        <button class="remove"><i class="fas fa-trash"></i></button>
+            <button class="done"><i class="fas fa-pen"></i></button>
+            <button class="remove"><i class="fas fa-trash"></i></button>
         </div>`
     };
 
